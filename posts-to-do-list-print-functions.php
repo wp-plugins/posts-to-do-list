a2f917d4d869282149bab4b5b27c6af117fbfebb
Ste_95 <Ste_95@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-04 20:03:58 +0000

    Version update - compatible WP 4.0
    git-svn-id: https://plugins.svn.wordpress.org/posts-to-do-list/tags/0.96@980406 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ posts-to-do-list-print-functions.php
@@ -0,0 +1,203 @@
+<?php
+
+class posts_to_do_list_print_functions extends posts_to_do_list_core {
+    
+function posts_to_do_list_print_new_item_form() {
+        global $wpdb, $wp_roles; ?>
+<div style="text-align: center;"><a href="#" title="Add new entry" id="posts_to_do_list_new">Add new post</a></div>
+<div id="posts_to_do_list_new_content" style="display: none;">
+    <br />
+    <div id="new_item_form">
+        <form method="post" id="new_item_form">
+            <label for="new_item_URL" style="font-weight: bold; font-size: bigger;">New item URL</label><br />
+            <input type="text" name="new_item_URL" id="new_item_URL" style="margin-bottom: 10px; width: 250px;" />
+            <br />
+            <label for="new_item_title" style="font-weight: bold; font-size: 1em;" id="new_item_title_label">New item title (mandatory)</label><br />
+            <input type="text" name="new_item_title" id="new_item_title" style="margin-bottom: 10px; width: 250px;" />
+            <br />
+            <label for="new_item_keyword" style="font-weight: bold; font-size: bigger; margin-bottom: 50px;">New item keyword</label><br />
+            <input type="text" name="new_item_keyword" id="new_item_keyword" style="margin-bottom: 10px; width: 250px;" />
+            <br />
+            <label for="new_item_notes" style="font-weight: bold; font-size: bigger;">New item notes</label><br />
+            <textarea rows="2" name="new_item_notes" id="new_item_notes" style="margin-bottom: 10px; width: 250px;" /></textarea>
+            <br />
+			<div style="margin-bottom: 5px;">
+				<label for="posts_to_do_list_author" style="font-weight: bold; font-size: bigger;">Assign post to specific user</label><br />
+				Role <select id="posts_to_do_list_roles" style="margin-top: 10px;">
+					<option value="0">Any</option>
+					
+					<?php
+					foreach( $wp_roles->role_names as $role => $role_name )
+						echo '<option value="'.$role.'">'.$role_name.'</option>';
+					?>
+					
+				</select>
+				
+			</div>
+			
+			User <select id="posts_to_do_list_author">
+                <option value="0">Unassigned</option>
+            
+        <?php $all_users = $wpdb->get_results( 'SELECT ID, display_name FROM '.$wpdb->users.' ORDER BY display_name ASC' );
+        foreach( $all_users as $single ) {
+            echo '<option value="'.$single->ID.'">'.stripslashes( $single->display_name ).'</option>';
+        } ?>
+
+            </select>
+            <br />
+            <br />
+            <label for="item_priority" style="font-weight: bold; font-size: bigger;">Set priority</label><br />
+            <select id="item_priority">
+                <option value="7">A matter of life or death</option>
+                <option value="6">Highest</option>
+                <option value="5">High</option>
+                <option value="4" selected="selected">Normal</option>
+                <option value="3">Low</option>
+                <option value="2">Lowest</option>
+                <option value="1">Lower than hell</option>
+            </select>
+            <br />
+            <div id="new_item_loading" style="display: none; float: right;"><img src="<?php echo parent::$posts_to_do_list_ajax_loader; ?>" alt="Loading..." title="Loading..." /></div>
+            <p style="float: left; color: red;" id="new_item_error"></p>
+            <span style="float: right;"><input type="submit" name="posts_to_do_list_new_submit" id="new_item_submit" value="Add" disabled="disabled" style="opacity: 0.50;" /></span>
+        </form>
+    </div>
+</div>
+    <?php }
+    
+    //After page data has been got and sorted, it is printed. If no rows are available, an information paragraph is shown
+    function posts_to_do_list_print_page( $selected_data ) {
+        
+        if( count( $selected_data ) == 0 ) {
+            echo '<p id="posts_to_do_list_no_posts_available">No posts available. Click on <em>Add new post</em> below to add a new item.</p>';
+        } else {
+            
+            foreach( $selected_data as $single ) {
+                posts_to_do_list_print_functions::posts_to_do_list_print_item( $single );
+            }
+        }
+    }
+    
+    //Prints each single item
+    function posts_to_do_list_print_item( $single ) {
+        global $current_user;
+        
+        $item_title         = stripslashes( $single->item_title );
+        $item_title_display = stripslashes( $single->item_title );
+        $item_title_style   = ' style="text-decoration: none;"'; //This is the default style
+        $item_url           = stripslashes( $single->item_url );
+        $item_date_added    = date( 'd/m/Y', $single->item_timestamp );
+        $item_adder         = get_userdata( $single->item_adder )->display_name;
+        $item_keyword       = stripslashes( $single->item_keyword );
+        $item_notes         = stripslashes( $single->item_notes );
+        $item_author        = 'Unassigned'; //This is the default assigment
+        $item_priority      = parent::posts_to_do_list_get_textual_priority( $single->item_priority );
+        $item_done_details  = @unserialize( $single->item_done );
+        
+        //If item is assigned to current user, highlight that
+        if( $current_user->ID == $single->item_author )
+            $item_title_display = '* '.$item_title;
+        
+        //If post was assigned to someone, fetch their display name
+        if( $single->item_author != 0 )
+            $item_author    = get_userdata( $single->item_author )->display_name;
+        
+        //If post has already been marked as done, check the related checkbox and strike the title
+        if( is_array( $item_done_details) ) {
+            $done_checked       = ' checked="checked"';
+            $item_title_style   = ' style="text-decoration: line-through;"';
+        } ?>
+        
+        <div class="item_to_do" style="margin-bottom: 5px;">
+            <strong><a class="item_to_do_link" href="<?php echo $item_url; ?>" title="<?php echo $item_title; ?>"<?php echo @$item_title_style; ?>><?php echo $item_title_display; ?></a></strong>
+            <div class="item_to_do_content" style="display: none; margin-left: 10px;">
+                <div class="content_left" style="margin-top: 2px;">
+                    <strong>Inserted</strong>: on <?php echo $item_date_added; ?> by <?php echo $item_adder; ?><br />
+                    <strong>Assigned to</strong>: <span class="assigned"><?php echo $item_author; ?></span><br />
+                    <strong>Priority</strong>: <?php echo $item_priority; ?><br />
+                    <strong>Keyword</strong>: <?php echo $item_keyword; ?><br />
+                    <strong>Notes</strong>: <?php echo $item_notes; ?><br />
+                
+        <?php if( is_array( $item_done_details ) )
+            echo '<p class="marked_as_done_p" style="font-style: italic;">This item was marked as done by '.get_userdata( $item_done_details['marker'] )->display_name.' on '.date( 'Y-m-d', $item_done_details['date'] ).'</p>'; ?>
+                
+                </div>
+                <div style="margin-top: 8px;">
+                    <div style="float: left; width: 50%;">
+                        &rArr; &nbsp;<a href="<?php echo admin_url().'post-new.php?post_title='.$item_title; ?>" title="Go to an already filled-in draft and start writing!">Write it!</a><br />
+                        
+        <?php if( strlen( $item_url ) > 0 ) { ?>
+                        
+                        &rArr; &nbsp;<a href="<?php echo $item_url; ?>" title="Go to source" target="_blank">Go to source</a><br />
+                        
+        <?php } else { ?>
+                        &rArr; &nbsp;No source
+        <?php } ?>
+                    </div>
+                    <div style="float: right; width: 50%;">
+                        &rArr; &nbsp;Mark as done <input type="checkbox" name="mark_as_done" class="mark_as_done" value="<?php echo $single->ID; ?>"<?php echo @$done_checked; ?> /><br />
+                        
+                        <?php if( $single->item_author != $current_user->ID ) { ?>
+                        
+                        &rArr; &nbsp;<a href="#" class="item_i_ll_take_it" title="If you plan to write this post, assign it to yourself so that other writers will not start writing it too" rel="<?php echo $single->ID; ?>">Assign to me</a>
+                        
+                        <?php } else if( $single->item_author == $current_user->ID AND array_intersect( parent::$posts_to_do_list_options['permission_item_unassign_roles'], get_userdata( $current_user->ID )->roles ) ) { ?>
+                        
+                        &rArr; &nbsp;<a href="#" class="item_i_dont_want_it_anymore" title="If you are not going to write this, you can leave it 'Unassigned'" rel="<?php echo $single->ID; ?>">Unassign from me</a>
+                        
+                            <?php } ?>
+                            
+                    </div>
+                    <div style="clear: both;"></div>
+                    <div style="margin: 0 auto; width: 50%">
+                    
+        <?php
+        //If current user belong to a user role that can delete items, show link
+        if( array_intersect( parent::$posts_to_do_list_options['permission_item_delete_roles'], get_userdata( $current_user->ID )->roles ) ) { ?>
+                        
+                        &rArr; &nbsp;<a href="#" class="item_delete" title="Delete this item" rel="<?php echo $single->ID; ?>">Delete item</a>
+                        
+        <?php } ?>
+                        
+                    </div>
+                </div>  
+            </div>
+            <div style="clear: both;"></div>
+        </div>
+        
+    <?php }
+    
+    function posts_to_do_list_print_detailed_stats( $detailed_stats_array ) {
+        $n = 0;
+        foreach( $detailed_stats_array as $single ) {
+            
+            if( $n % 2 == 0 )
+                $row_alternate = ' class="alternate"';
+            
+            echo '<tr'.@$row_alternate.'>
+            <td>'.$single['display_name'].'</td>
+            <td>'.$single['added_items'].'</td>
+            <td>'.$single['assigned_items'].'</td>
+            <td>'.$single['total_marked_as_done_items'].'</td>
+            <td>'.$single['assigned_marked_as_done_items'].'</td>
+            <td>'.$single['still_to_do_items'].'</td>
+            <td>'.$single['created_posts'].'</td>
+            <td>'.$single['published_posts'].'</td>';
+            
+            unset( $row_alternate );
+            ++$n;
+        }
+    }
+    
+    function posts_to_do_list_print_general_stats( $general_stats_array ) {
+        echo '<tr>
+            <td>'.$general_stats_array['added_items'].'</td>
+            <td>'.$general_stats_array['marked_as_done_items'].'</td>
+            <td>'.$general_stats_array['still_to_do_items'].'</td>
+            <td>'.$general_stats_array['created_posts'].'</td>
+            <td>'.$general_stats_array['published_posts'].'</td>';
+    }
+    
+}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,72 @@
+﻿=== Posts To Do List ===
+Contributors: Ste_95
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=4Y4STCU56MUKE
+Tags: multi author, post management, to do list, posts
+Tested up to: 4.0
+Stable tag: 0.96
+Requires at least: 3.0
+
+Share post ideas with writers, suggest them writing topics and keep track of the posts ideas with a to do list.
+
+== Description ==
+Most people who run a multi-author blog need to tell their writers what post they should write. And so administrators send emails with a URL of the post source, the keyword for the post, any notes accompanying that. And sometimes a user notices a post that is worth writing and he wants to tell the other writers, so they have to email the administrator who will tell the other users and everyone will kill themselves in the end. The Posts To Do List plugin allows you to get rid of that rigmarole.
+
+By a convenient box in the posts editing page, **everyone will be able to share the posts they think are worth writing. You have this little box, where you put the URL of the page where you read that great post, and the plugin will fetch the title by itself. You will then be able to change the retrieved title, suggest a keyword and add other notes, set a priority and assign the post to some user of the blog.** And if you want to leave everything blank but the title field, leaving a suggestion that anyone can catch and deepen... well, you can!
+
+No more emails to tell "You do this and that, use this keyword and don't forget that...". Everything can be down inside Wordpress. Not only by the administrator, but by any logged in user.
+
+You, as the administrator, want everything in the power of your hand? No problem, you can **decide what user roles can add new posts to the to do list and what user roles can delete already added items.** You want your users to stick to the post you assigned to them? You can hide the posts you have assigned to other users from their view. From a **simple stats page** it will be immediately clear how many posts you have already assigned and how many of them are still to do, so that it will be easy to understand how much your writers have done and how many posts you have still to assign. Almost every action is powered by AJAX, so that no page reloads are needed and you do not even notice it is happening, it just works. What do you want more? Cause we even have cookies!
+
+Posts To Do List was reviewed by:
+  
+* [IdeaGeek](http://www.ideageek.it/organizzare-post-wordpress-facile-con-posts-to-do-list "IdeaGeek")
+* [Mondofico](http://www.mondofico.com/2012/09/gestione-post-da-fare-wordpress-posts-to-do-list "Mondofico")
+* [Wordpress Style](http://www.wpstyle.it/plugin-wordpress/gestire-i-post-da-fare-con-posts-to-do-list.html "Wordpress Style")
+* [WpCode.net ENG](http://www.wpcode.net/to-do-list.html/ "WpCode.net ENG")
+
+== Installation ==
+1. Upload the directory of the Posts To Do List in your wp-content/plugins directory; note that you need the whole folder, not only the single files.
+2. Activate the plugin through the "Activate" button in the "Plugins" page of your WordPress.
+3. Head to the configuration page first. The plugin already comes with a predefined set of settings, but you may want to configure it to better suit your needs.
+5. That's it, done!
+
+== Changelog ==
+= 0.9.6 =
+* New option to prevent non-admins from claiming posts already assigned to other users (works in real time: if a post is claimed and another user tries to claim one second later because it still displays as unassigned, an error occurs).
+* New option to prevent non-admins from claiming a post if they have not completed their latest assignment.
+
+= 0.9.5 =
+* When assigning items, users can be filtered by user role.
+
+= 0.94 =
+* Dashboard page now visible to all users with the capability edit_posts.
+
+= 0.9.3 =
+* Fixed a bug which resulted in a fatal error due to PHP 5.4 incompatibility. Sorry!
+
+= 0.9.2 =
+* Updated the plugin URI, which pointed to a non-existent page.
+
+= 0.9.1 =
+* Bugfix: AJAX actions may have triggered PHP warning because of Wordpress change of policy with wpdb->prepare() (actions were still executed, though).
+* Bugfix: new installations missed an option, which resulted in a PHP warning in the Options page for the roles able to unassing items. If you experience this, just check a role for that permission, save, then uncheck it and save again. 
+
+= 0.9 =
+* A new button in the options page allows to delete all the already marked as done items.
+
+= 0.8.5 =
+* If enabled in the plugin options page, when a newly added post is assigned to a certain user, an email is automatically sent to them with the details of the new item.
+
+= 0.8 =
+* Users can now assign to themselves posts added to the list as unassigned and assigned to other users.
+* Users can now unassign from them posts assigned to themselves.
+* A new permission allow to define what user role can unassign posts from themselves.
+* Actions that need a reload of the list because of list sorting not get that reloading (deleting, assigning and unassigning).
+
+= 0.7 =
+* First release.
+
+== Screenshots ==
+1. Posts To Do List dashboard page. The provided datapicker allows to edit the time range and select the wished stats
+2. Posts To Do List configuration page
+3. New post page/edit post page, with the Posts To Do List box. The "Add new post" part is collapsible.
